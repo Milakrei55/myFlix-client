de06092f1c846bfab54b167edb20cc006dcb7daa
Milakrei55 <mischakom@gmail.com>
2025-04-16 23:57:46 +0200

    styling with React Bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,28 +1,30 @@
 import PropTypes from "prop-types";
+import { Card, Button } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.title}
-    </div>
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.director}</Card.Text>
+        <Card.Text>{movie.description}</Card.Text>
+        <Card.Text>{movie.genre}</Card.Text>
+        <Button onClick={() => onMovieClick(movie)} variant="link">
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
-MovieCard.proptypes = {
+MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string,
-    Image: PropTypes.string.isRequired,
-    Director: PropTypes.shape({
-      Name: PropTypes.string,
-    }).isRequired,
-    Description: PropTypes.string,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string,
-    }),
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    director: PropTypes.string,
+    description: PropTypes.string,
+    genre: PropTypes.string,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,22 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
+import Container from "react-bootstrap/Container";
 
 import "./index.scss";
 
+
+//Main component
 const App = () => {
-  return <MainView />;
+  return(
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
 
+// Finds the root the app
 const container = document.querySelector("#root");
 const root = createRoot(container);
+
+// Tells React to render the app in the root DOM element
 root.render(<App />);
